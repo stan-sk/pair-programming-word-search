6ca5cb5b041a37d4e594a418dee3793fb44c9f05
Stanley Lai <stanley.lai@telus.com>
2022-11-23 15:11:03 -0800

    complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
+    
+  let verticalJoin = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+      const col =[]
+      for(let j = 0; j < letters.length; j++) {
+          col.push(letters[j][i]);
+        }
+        verticalJoin.push(col);
+    }
+    verticalJoin = verticalJoin.map(ls => ls.join(''));
+
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    const thirdArray = verticalJoin.concat(horizontalJoin);
+    console.log(thirdArray);
+
+    for (l of thirdArray) {
+        if (l.includes(word)) return true;
+    }
+    for (j of thirdArray)   {
+        
+        if (!l.includes(word)) return false;
     }
 }
 
 module.exports = wordSearch
+
+
+
+
+
+
+
+
+
